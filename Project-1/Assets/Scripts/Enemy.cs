9ac0f2ec800de7421f9e404b7e20a002ff1558dd
krilliam-shrimpley <Arizonatreaman@gmail.com>
2025-06-19 23:05:10 -0700

    statue and enemy nav done also theyre all shrimp

--- Project-1/Assets/Scripts/Enemy.cs
+++ Project-1/Assets/Scripts/Enemy.cs
@@ -1,16 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Enemy Target Settings")]
+    public Transform target;
+    
+    private NavMeshAgent _navMeshAgent;
+    private float _distanceToTarget;
+    
+   
+    // Start is called before the first frame update
     void Start()
     {
-        
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _distanceToTarget = Vector3.Distance(_navMeshAgent.transform.position, target.position);
+
+        _navMeshAgent.destination = target.position;
     }
 }
